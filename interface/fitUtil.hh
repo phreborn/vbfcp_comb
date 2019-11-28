383f75ffc798b36a0aaee63eac90234317ee5851
Hongtao Yang <Hongtao.Yang@cern.ch>
2019-11-28 14:06:28 +0100

    Adding missing files

---
+++ interface/fitUtil.hh
@@ -0,0 +1,26 @@
+#ifndef FITUTIL_HEADER
+#define FITUTIL_HEADER
+
+#include "CommonHead.h"
+#include "RooFitHead.h"
+#include "RooStatsHead.h"
+
+#include "auxUtil.hh"
+
+using namespace std;
+using namespace RooFit;
+using namespace RooStats;
+
+class fitUtil{
+public:
+  // Minimizer setup
+  static int _minimizerStrategy;
+  static string _minimizerAlgo;
+  static double _minimizerTolerance;
+  static bool _nllOffset;
+  static int _printLevel;
+
+  static int profileToData(ModelConfig *mc, RooAbsData *data, TString rangeName="");
+};
+
+#endif
